228f9bce4dfd69987d319d822483fe0cf1064075
mahdilow <mahdi.loravand2002@gmail.com>
2023-08-06 17:44:15 +0430

    no msg

--- pcshop/settings.py
+++ pcshop/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'pcshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'data/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# for lira data/
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
