951853f4d4543855028a5f6000383416219b5c9c
mahdilow <mahdi.loravand2002@gmail.com>
2024-11-08 15:28:54 +0330

    Update settings.py

--- pcshop/settings.py
+++ pcshop/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 import os
 
 
 
-load_dotenv('')
+#load_dotenv(find_dotenv())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
